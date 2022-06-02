7ec7f3ca846bd8dd372a468dcb88401848a41f34
DESKTOP-PP1Q6F9\user <DESKTOP-PP1Q6F9\user>
2022-06-02 17:17:27 +0900

    2022-06-02 type finish

---
+++ type.js
@@ -0,0 +1,23 @@
+// number
+
+let n = 123;
+n = 12.345;
+
+Infinity // 특수 숫자값
+-Infinity // 특수 숫자값
+NaN // 특수 숫자값
+
+
+// BigInt
+
+const bigInt = 1231232984903284902834902n // 길이에 상관없이 정수를 나타냄. IE에선 지원 X
+
+// string
+
+let str = "Hello";
+let str2 = 'single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+console.log(phrase);
+
+
--- varLetConst.js
+++ varLetConst.js
@@ -6,6 +6,10 @@ message = 'hello';
 // let message = 'yaho';
 
 let user = 'woogie',
-    age = 25;,
+    age = 25,
     message = 'yaho'
+
+// 이렇게도 선언가능
+
+// 두번선언하면 에러뱉음
 
