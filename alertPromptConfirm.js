fbcde5ed62f3735477c385c6a679f892d98849bd
DESKTOP-PP1Q6F9\user <kkimdo20>
2022-06-03 08:56:20 +0900

    2022-06-03 type finish

--- alertPromptConfirm.js
+++ alertPromptConfirm.js
@@ -1,3 +1,9 @@
 alert('ok'); // alert 함수는 확인 버튼을 누를 때까지 메시지를 보여주는 창이 떠있게 된다.
 
-let result = prompt('title', 100); // prompt 함수는 입력필드에 기재한 문자열을 반환함.
+// let result = prompt('title', 100); // prompt 함수는 입력필드에 기재한 문자열을 반환함.
+
+// result = confirm(question);
+
+let isBoss = confirm("당신이 주인인가?");
+
+alert(isBoss); // 확인 취소 택일
