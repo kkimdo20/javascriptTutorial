1a68620696492f175752c40d1ef59b0350d28d81
DESKTOP-PP1Q6F9\user <DESKTOP-PP1Q6F9\user>
2022-06-02 15:01:22 +0900

    2022-06-02

---
+++ errorEx.js
@@ -0,0 +1,12 @@
+
+// error version
+
+// alert("error gogo")
+
+// [1, 2].forEach(alert)
+
+// no error vversion
+
+alert("error gogo");
+
+[1,2].forEach(alert)
---
+++ varLetConst.js
@@ -0,0 +1,11 @@
+
+message = 'hello';
+
+// let user = 'woogie';
+// let age = 25;
+// let message = 'yaho';
+
+let user = 'woogie',
+    age = 25;,
+    message = 'yaho'
+
